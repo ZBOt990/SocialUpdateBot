547a0c1f1631d2057facbd118b5546c32f2306a1
Muhammad Dawood <92998911+ZBOt990@users.noreply.github.com>
2023-10-22 19:07:44 +0500

    add code

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-// the config for all the commands and setup
+const { Client, GatewayIntentBits } = require('discord.js');
+const config = require('./config.json');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+});
+
+client.once('ready', () => {
+  console.log('Logged in as ' + client.user.tag);
+  
+  const discordChannel = client.channels.cache.get(config.discordChannelId);
+  const instagramChannel = client.channels.cache.get(config.instagramChannelId);
+
+  if (discordChannel && instagramChannel) {
+    setInterval(() => {
+      const memberCount = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+      const instagramFollowers = getInstagramFollowers(); 
+
+      discordChannel.setName(`Discord: ${formatNumber(memberCount)}`);
+      instagramChannel.setName(`Instagram: ${formatNumber(instagramFollowers)}`);
+    }, 600000); 
+  } else {
+    console.error('Invalid channel IDs in config.json');
+  }
+});
+
+function formatNumber(number) {
+  if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + 'M';
+  } else if (number >= 1000) {
+    return (number / 1000).toFixed(1) + 'K';
+  } else {
+    return number.toString();
+  }
+}
+
+function getInstagramFollowers() {
+  return 11111111111111111111111111111111
+}
+
+client.login(config.token);
